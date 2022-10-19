aab10dd237fa10526d5b92ecad04f1273cc579fa
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2022-10-19 10:29:20 +0200

    Topological sort of `let` statements (#11)
    Fixes #10

--- lets_expect_core/src/core/create_test.rs
+++ lets_expect_core/src/core/create_test.rs
@@ -1,20 +1,48 @@
 use proc_macro2::{Ident, TokenStream};
 use quote::quote_spanned;
 
-use super::runtime::Runtime;
+use super::{
+    runtime::Runtime,
+    topological_sort::{topological_sort, TopologicalSortError},
+};
 
 pub fn create_test(identifier: &Ident, runtime: &Runtime, content: &TokenStream) -> TokenStream {
     let content = content;
-    let lets = &runtime.lets;
+    let lets = topological_sort(&runtime.lets);
+
+    let lets = match lets {
+        Ok(lets) => lets,
+        Err(error) => {
+            return match error {
+                TopologicalSortError::CyclicDependency => {
+                    quote_spanned! { identifier.span() =>
+                        compile_error!("Cyclic dependency between variables detected");
+                    }
+                }
+                TopologicalSortError::IdentExpected => {
+                    quote_spanned! { identifier.span() =>
+                        compile_error!("Expected an identifier in `let`");
+                    }
+                }
+            }
+        }
+    };
+
+    let befores = &runtime.befores;
+    let afters = &runtime.afters;
 
     quote_spanned! { identifier.span() =>
         #[test]
         fn #identifier() -> Result<(), TestFailure> {
             #(#lets)*
 
+            #(#befores)*
+
             let test_cases = {
                 #content
             };
+
+            #(#afters)*
 
             test_result_from_cases(test_cases)
         }
--- lets_expect_core/src/core/to_ident.rs
+++ lets_expect_core/src/core/to_ident.rs
@@ -1,6 +1,9 @@
 use english_numbers::{convert, Formatting};
 use proc_macro2::Ident;
-use syn::{Stmt, BinOp, punctuated::Punctuated, Expr, token::Comma, ExprAssign, Member, ExprRange, Lit, Local, Pat, UnOp, Type};
+use syn::{
+    punctuated::Punctuated, token::Comma, BinOp, Expr, ExprAssign, ExprRange, Lit, Local, Member,
+    Pat, Stmt, Type, UnOp,
+};
 
 pub fn stmt_to_ident(stmt: &Stmt) -> String {
     match stmt {
@@ -13,36 +16,70 @@ pub fn stmt_to_ident(stmt: &Stmt) -> String {
 
 pub fn local_to_ident(local: &Local) -> String {
     let ident = pat_to_ident(&local.pat).to_string();
-    ident + "_is_" + expr_to_ident(&local.init.as_ref().expect("When `let` statements are expected to have an initial value").1).as_str()
+    ident
+        + "_is_"
+        + expr_to_ident(
+            &local
+                .init
+                .as_ref()
+                .expect("When `let` statements are expected to have an initial value")
+                .1,
+        )
+        .as_str()
 }
 
 pub fn pat_to_ident(pat: &Pat) -> Ident {
     match pat {
-        Pat::Ident(ref pat_ident) => pat_ident.ident.clone(),
-        _ => todo!("Expected Pat::Ident, got {:?}", pat),
+        Pat::Ident(pat) => pat.ident.clone(),
+        Pat::Type(pat) => pat_to_ident(&pat.pat),
+        _ => todo!("Unable to convert pattern to identifier"),
     }
 }
 
 pub fn expr_to_ident(expr: &syn::Expr) -> String {
     match expr {
-        Expr::Array(array) => if array.elems.is_empty() { "empty_array".to_string() } else { punctuated_to_ident(&array.elems)},
+        Expr::Array(array) => {
+            if array.elems.is_empty() {
+                "empty_array".to_string()
+            } else {
+                punctuated_to_ident(&array.elems)
+            }
+        }
         Expr::Assign(assign) => expr_assign_to_ident(assign),
-        Expr::AssignOp(assign_op) => expr_to_ident(&assign_op.left) + "_" + binary_op_to_ident(&assign_op.op) + "_" + &expr_to_ident(&assign_op.right),
+        Expr::AssignOp(assign_op) => {
+            expr_to_ident(&assign_op.left)
+                + "_"
+                + binary_op_to_ident(&assign_op.op)
+                + "_"
+                + &expr_to_ident(&assign_op.right)
+        }
         Expr::Async(_) => todo!("Async not supported"),
         Expr::Await(_) => todo!("Await not supported"),
-        Expr::Binary(binary) => expr_to_ident(&binary.left) + "_" + binary_op_to_ident(&binary.op) + "_" + &expr_to_ident(&binary.right),
+        Expr::Binary(binary) => {
+            expr_to_ident(&binary.left)
+                + "_"
+                + binary_op_to_ident(&binary.op)
+                + "_"
+                + &expr_to_ident(&binary.right)
+        }
         Expr::Block(block) => block_to_ident(block),
         Expr::Box(boxed) => "boxed_".to_string() + &expr_to_ident(&boxed.expr),
         Expr::Break(_) => todo!("Break not supported"),
-        Expr::Call(call) => expr_to_ident(&call.func) + if !call.args.is_empty() {
-            "_".to_string() + punctuated_to_ident(&call.args).as_str()
-        } else {
-            "".to_string()
-        }.as_str(),
+        Expr::Call(call) => {
+            expr_to_ident(&call.func)
+                + if !call.args.is_empty() {
+                    "_".to_string() + punctuated_to_ident(&call.args).as_str()
+                } else {
+                    "".to_string()
+                }
+                .as_str()
+        }
         Expr::Cast(cast) => expr_to_ident(&cast.expr) + "_as_" + &type_to_ident(&cast.ty),
         Expr::Closure(_) => todo!("Closure not supported"),
         Expr::Continue(_) => todo!("Continue not supported"),
-        Expr::Field(field) => expr_to_ident(&field.base) + "_" + member_to_ident(&field.member).as_str(),
+        Expr::Field(field) => {
+            expr_to_ident(&field.base) + "_" + member_to_ident(&field.member).as_str()
+        }
         Expr::ForLoop(_) => todo!("ForLoop not supported"),
         Expr::Group(_) => todo!("Group not supported"),
         Expr::If(if_expr) => "if_".to_string() + &expr_to_ident(&if_expr.cond),
@@ -52,11 +89,17 @@ pub fn expr_to_ident(expr: &syn::Expr) -> String {
         Expr::Loop(_) => todo!("Loop not supported"),
         Expr::Macro(mac) => mac.mac.path.get_ident().unwrap().to_string().to_lowercase(),
         Expr::Match(_) => "match".to_string(),
-        Expr::MethodCall(method_call) => expr_to_ident(&method_call.receiver) + "_" + method_call.method.to_string().to_lowercase().as_str() + if !method_call.args.is_empty() {
-            "_".to_string() + punctuated_to_ident(&method_call.args).as_str()
-        } else {
-            "".to_string()
-        }.as_str(),
+        Expr::MethodCall(method_call) => {
+            expr_to_ident(&method_call.receiver)
+                + "_"
+                + method_call.method.to_string().to_lowercase().as_str()
+                + if !method_call.args.is_empty() {
+                    "_".to_string() + punctuated_to_ident(&method_call.args).as_str()
+                } else {
+                    "".to_string()
+                }
+                .as_str()
+        }
         Expr::Paren(paren) => expr_to_ident(&paren.expr),
         Expr::Path(path) => path_to_ident(path),
         Expr::Range(range) => range_to_ident(range),
@@ -68,7 +111,9 @@ pub fn expr_to_ident(expr: &syn::Expr) -> String {
         Expr::TryBlock(_) => todo!("TryBlock not supported"),
         Expr::Tuple(_) => todo!("Tuple not supported"),
         Expr::Type(_) => todo!("Type not supported"),
-        Expr::Unary(unary) => unary_op_to_ident(&unary.op).to_string() + "_" + &expr_to_ident(&unary.expr),
+        Expr::Unary(unary) => {
+            unary_op_to_ident(&unary.op).to_string() + "_" + &expr_to_ident(&unary.expr)
+        }
         Expr::Unsafe(_) => todo!("Unsafe not supported"),
         Expr::Verbatim(_) => todo!("Verbatim not supported"),
         Expr::While(_) => todo!("While not supported"),
@@ -79,7 +124,11 @@ pub fn expr_to_ident(expr: &syn::Expr) -> String {
 
 fn type_to_ident(ty: &Type) -> String {
     match ty {
-        Type::Array(array) => format!("array_of_{}_{}", expr_to_ident(&array.len), type_to_ident(&array.elem)),
+        Type::Array(array) => format!(
+            "array_of_{}_{}",
+            expr_to_ident(&array.len),
+            type_to_ident(&array.elem)
+        ),
         Type::BareFn(_) => todo!(),
         Type::Group(_) => todo!(),
         Type::ImplTrait(_) => todo!(),
@@ -87,7 +136,14 @@ fn type_to_ident(ty: &Type) -> String {
         Type::Macro(_) => todo!(),
         Type::Never(_) => todo!(),
         Type::Paren(_) => todo!(),
-        Type::Path(path) => path.path.segments.last().unwrap().ident.to_string().to_lowercase(),
+        Type::Path(path) => path
+            .path
+            .segments
+            .last()
+            .unwrap()
+            .ident
+            .to_string()
+            .to_lowercase(),
         Type::Ptr(_) => todo!(),
         Type::Reference(_) => todo!(),
         Type::Slice(_) => todo!(),
@@ -99,11 +155,21 @@ fn type_to_ident(ty: &Type) -> String {
 }
 
 pub fn path_to_ident(path: &syn::ExprPath) -> String {
-    path.path.segments.last().expect("Expected path to have at least 1 segment").ident.to_string().to_lowercase()
+    path.path
+        .segments
+        .iter()
+        .map(|segment| segment.ident.to_string().to_lowercase())
+        .collect::<Vec<String>>()
+        .join("_")
 }
 
 fn block_to_ident(block: &syn::ExprBlock) -> String {
-    block.block.stmts.last().map(stmt_to_ident).unwrap_or_else(|| "noop".to_string())
+    block
+        .block
+        .stmts
+        .last()
+        .map(stmt_to_ident)
+        .unwrap_or_else(|| "noop".to_string())
 }
 
 fn member_to_ident(member: &Member) -> String {
@@ -124,7 +190,11 @@ fn expr_assign_to_ident(assign: &ExprAssign) -> String {
 }
 
 pub fn punctuated_to_ident(punctuated: &Punctuated<Expr, Comma>) -> String {
-    punctuated.iter().map(expr_to_ident).collect::<Vec<String>>().join("_")
+    punctuated
+        .iter()
+        .map(expr_to_ident)
+        .collect::<Vec<String>>()
+        .join("_")
 }
 
 // pub fn punctuated_assignments_to_ident(punctuated: &Punctuated<Assignment, Comma>) -> String {
@@ -137,7 +207,14 @@ fn expr_lit_to_ident(lit: &syn::ExprLit) -> String {
         Lit::ByteStr(_) => todo!(),
         Lit::Byte(_) => todo!(),
         Lit::Char(_) => todo!(),
-        Lit::Int(value) => convert(value.base10_parse::<i64>().unwrap(), Formatting { dashes: false, title_case: false, ..Formatting::default() }),
+        Lit::Int(value) => convert(
+            value.base10_parse::<i64>().unwrap(),
+            Formatting {
+                dashes: false,
+                title_case: false,
+                ..Formatting::default()
+            },
+        ),
         Lit::Float(_) => todo!(),
         Lit::Bool(value) => value.value.to_string(),
         Lit::Verbatim(_) => todo!(),
